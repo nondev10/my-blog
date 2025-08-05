b4d8ea6a55b59e78716cbc335979de76c1b6124f
nondev10 <131510050+nondev10@users.noreply.github.com>
2025-08-05 23:52:54 +0800

    Update test-update

---
+++ new-post.bat
@@ -0,0 +1,17 @@
+@echo off
+git pull
+git remote set-url origin git@github.com:nondev10/my-blog.git
+
+:: Create
+set /p mark="Press article keywords("-^" to link): "
+echo Run it by yourself: pnpm new-post %mark%
+pause > nul
+explorer ".\src\content\posts\%mark%.md"
+
+pause > nul
+
+:: Commit
+git commit -am "Update %mark%"
+git push
+
+pause > nul
---
+++ src/content/posts/test-update.md
@@ -0,0 +1,12 @@
+---
+title: test-update
+published: 2025-08-05
+description: '一个测试'
+image: ''
+tags: ['测试']
+category: '记录'
+draft: false 
+lang: ''
+---
+
+This is a test for update posts.
